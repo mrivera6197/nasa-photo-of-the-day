6592d30482f872c8c2505a99cae713c34b52be9b
Ben Rivera <benajmin.p.rivera@vanderbilt.edu>
2020-10-08 18:52:12 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import "./App.css";
 
 import axios from 'axios'
 import Photo from './components/Photo'
+import PhotoGallery from './components/PhotoGallery'
 
 function App() {
 
   const [photo, setPhoto] = useState([])
+  const [photoGallery, setPhotoGallery] = useState([])
+  const [currentPhoto, setCurrentPhoto] = useState('')
 
+  const openDetails = id => {
+    setCurrentPhoto(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentPhoto(null)
+  }
+ 
   useEffect (() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=ppwlJpfYFZ1kyC8OJGEheSBJECZYMTyxNuR793hl')
       .then(res => {
@@ -19,14 +30,31 @@ function App() {
       })
   }, [])
 
+  useEffect (() => {
+    axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=ppwlJpfYFZ1kyC8OJGEheSBJECZYMTyxNuR793hl')
+      .then(res => {
+        const photoArray = res.data.photos
+        let roverArray = []
+        photoArray.forEach(image => {
+          roverArray.push(image.img_src)
+        }, [])
+        setPhotoGallery(roverArray)
+      }, [])
+      .catch(err => {
+        debugger
+      })
+  }, [])
+
   return (
-    <div className="App">
+    <div className='App'>
       <div className='App-header'> 
         <p>header</p>
       </div>
       <div> 
-        <h1 className='main-title'>NASA Image of the Day</h1>
         <Photo photo={photo}/>
+      </div>
+      <div className='photoGallery'>
+        <PhotoGallery photoGallery={photoGallery} />
       </div>
     </div>
   );
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,21 +1,45 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import "../App.css";
+
+const StyledPhoto = styled.div `
+    
+    .dailyPhoto img {
+        width: 70%;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    .main-title {
+        text-align: start;
+        margin: 5%;
+        font-size: 1.5rem;
+    }
+    .photoInfo {
+        margin: 5% 20% 5%;
+        line-height: 1.6;
+    }
+`
 
 const photoOfDay = ({photo}) => {
     console.log(photo.url)
     return (
-    <div>
+    <StyledPhoto>
+        <h1 className='main-title'>NASA Image of the Day</h1>
         <h2 className='subtitle'>{photo.title}</h2>
         <div> 
             <div className='dailyPhoto'>
                 <img src={photo.url} alt={photo.explanation}/> 
             </div>
+            <button className='button'>
+                See Details
+            </button> 
             <div className='photoInfo'> 
-                <p>{photo.explanation}</p>
-                <p>Photo date: {photo.date}</p>
-                <p>Copyright: {photo.copyright}</p>
+                    <p>{photo.explanation}</p>
+                    <p>Photo date: {photo.date}</p>
+                    <p>Copyright: {photo.copyright}</p>
             </div>
+            
         </div>
-    </div>
+    </StyledPhoto>
     )
 }
 
